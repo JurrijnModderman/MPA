d78e59c96cb76562a9fc28802283f7662b00a957
Unknown <99053654@mydavinci.nl>
2021-07-06 11:24:37 +0200

    added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'ProductsController@index')->name('product');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+require __DIR__ . '/auth.php';
+
