bde2f78156bd6beae4c0577fa05388604c00c5cc
Unknown <99053654@mydavinci.nl>
2021-07-07 15:42:40 +0200

    route doesnt work

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,28 +9,20 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          User
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="">Login</a>
+          <a class="dropdown-item" href="">Register</a>
         </div>
       </li>
       <li class="nav-item">
@@ -45,8 +37,8 @@
   <div class="card-body">
     <h5 class="card-title">{{$product->title}}</h5>
     <p class="card-text">{{$product->description}}</p>
-    <p class="card-text">{{$product->price}}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">€{{$product->price}}-,</p>
+    <a href="#" class="btn btn-primary">Add to cart</a>
   </div>
 </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,12 @@ Route::get('/', 'ProductsController@index')->name('product');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('/login', function () {
+    return view('/login');
+});
+Route::get('/register', function () {
+    return view('/register');
+});
 
 require __DIR__ . '/auth.php';
 
