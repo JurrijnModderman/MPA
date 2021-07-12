b27472c809d6cd57b56e06c36403d276ac3438b5
Unknown <99053654@mydavinci.nl>
2021-07-12 16:15:28 +0200

    made the checkoutpage a bit

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -31,10 +31,10 @@ class ProductsController extends Controller
 
     public function getCart() {
         if (!Session::has('cart')) {
-            return view('shop.shopping-cart', ['products' => null]);
+            return view('shopping-cart', ['products' => null]);
         }
         $oldCart = Session::get('cart');
         $cart =  new Cart($oldCart);
-        return view('shop.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+        return view('shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
 }
