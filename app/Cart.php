7294b6ab1911faef9a44c7a2aa7c7e124cec672e
Unknown <99053654@mydavinci.nl>
2021-07-14 12:13:17 +0200

    using mammp now, made the db again and migrated it

--- app/Cart.php
+++ app/Cart.php
@@ -2,7 +2,7 @@
  namespace App;
 
  class Cart {
-     public $items = null;
+     public $items = [];
      public $totalQty = 0;
      public $totalPrice = 0;
 
@@ -13,19 +13,24 @@
              $this->totalQty = $oldCart->totalQty;
              $this->totalPrice = $oldCart->totalPrice;
          }
+         $this->save();
      }
+     public function save()
+    {
+        session()->put('cart', $this);
+    }
 
-     public function add($item, $id) {
-         $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
-         if ($this->items) {
-             if (array_key_exists($id, $this->items)) {
-                 $storedItem = $this->items[$id];
-             }
-         }
-         $storedItem['qty']++;
-         $storedItem['price'] = $item->price * $storedItem['qty'];
-         $this->items[$id] = $storedItem;
-         $this->totalQty++;
-         $this->totalPrice += $item->price;
-     }
+    public function add($item, $id)
+    {
+        $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
+        if ($this->items && array_key_exists($id, $this->items)) {
+            $storedItem = $this->items[$id];
+        }
+        $storedItem['qty']++;
+        $storedItem['price'] = $item->price * $storedItem['qty'];
+        $this->items[$id] = $storedItem;
+        $this->totalQty++;
+        $this->totalPrice += $item->price;
+        $this->save();
+    }
  }
